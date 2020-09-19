2583d6784e3c0bee1d5e6b60b68a512efda12405
Erlon Jr <ejrgeek@gmail.com>
2020-09-19 13:59:54 -0300

    create model Schedule

--- backend/apps/schedule/models.py
+++ backend/apps/schedule/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from apps.doctor.models import Doctor
 # Create your models here.
+
+
+class Schedule(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    day = models.DateField(blank=False, null=False)
+    schedules = models.TimeField(blank=False, null=False)
