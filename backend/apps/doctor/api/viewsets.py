cec3e2e1a60ce9c1a0a739909318c01936893830
Erlon Jr <ejrgeek@gmail.com>
2020-09-30 19:11:18 -0300

    params filters in doctors

--- backend/apps/doctor/api/viewsets.py
+++ backend/apps/doctor/api/viewsets.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from .serializers import DoctorSerializer
@@ -5,14 +7,43 @@ from apps.doctor.models import Doctor
 from rest_framework.permissions import IsAuthenticated
 
 
-class DoctorViewSet(ModelViewSet):
+def format_query_params(query):
+    try:
+        query = query.strip('/api/medicos/')
+        query = query.replace('search', 'name') \
+            .replace('especialidade', 'specialty') \
+            .replace('=', '":"') \
+            .replace('?', '{"') \
+            .replace('&', '","')
+        query = query
+        query += '"}'
+        query = json.loads(query)
+        return query
+    except Exception:
+        return {}
 
+
+class DoctorViewSet(ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
     permission_classes = [IsAuthenticated]
     http_method_names = ['get']
 
     def list(self, request, *args, **kwargs):
+        query = request.get_full_path()
+        query = format_query_params(query)
         queryset = Doctor.objects.all()
         serializer = DoctorSerializer(queryset, many=True)
-        return Response(serializer.data)
+
+        if len(query) > 0:
+            doctors = Doctor.objects \
+                .filter(name__icontains=query['name']) \
+                .filter(specialty=query['specialty'])
+            serializer = DoctorSerializer(doctors, many=True)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.data)
+
+
+
+
