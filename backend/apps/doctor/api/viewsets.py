a76956a78e2adab88c13319b40626d4338ba86cc
Erlon Jr <ejrgeek@gmail.com>
2020-09-30 21:37:26 -0300

    fix filters in doctors and add filters in schedules

--- backend/apps/doctor/api/viewsets.py
+++ backend/apps/doctor/api/viewsets.py
@@ -1,26 +1,20 @@
-import json
-
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
-from .serializers import DoctorSerializer
-from apps.doctor.models import Doctor
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from apps.doctor.models import Doctor
+from .serializers import DoctorSerializer
 
 
-def format_query_params(query):
-    try:
-        query = query.strip('/api/medicos/')
-        query = query.replace('search', 'name') \
-            .replace('especialidade', 'specialty') \
-            .replace('=', '":"') \
-            .replace('?', '{"') \
-            .replace('&', '","')
-        query = query
-        query += '"}'
-        query = json.loads(query)
-        return query
-    except Exception:
-        return {}
+def return_query(param):
+    if 'search' in param.keys() and 'especialidade' in param.keys():
+        return Doctor.objects \
+            .filter(name__icontains=param['search']) \
+            .filter(specialty=param['especialidade'])
+    elif 'search' in param.keys():
+        return Doctor.objects.filter(name__icontains=param['search'])
+    elif 'especialidade' in param.keys():
+        return Doctor.objects.filter(specialty=param['especialidade'])
 
 
 class DoctorViewSet(ModelViewSet):
@@ -30,20 +24,14 @@ class DoctorViewSet(ModelViewSet):
     http_method_names = ['get']
 
     def list(self, request, *args, **kwargs):
-        query = request.get_full_path()
-        query = format_query_params(query)
+        querys = request.query_params
         queryset = Doctor.objects.all()
         serializer = DoctorSerializer(queryset, many=True)
 
-        if len(query) > 0:
-            doctors = Doctor.objects \
-                .filter(name__icontains=query['name']) \
-                .filter(specialty=query['specialty'])
+        if len(querys) > 0:
+            doctors = {}
+            doctors = return_query(querys)
             serializer = DoctorSerializer(doctors, many=True)
             return Response(serializer.data)
         else:
             return Response(serializer.data)
-
-
-
-
--- backend/apps/schedule/api/viewsets.py
+++ backend/apps/schedule/api/viewsets.py
@@ -1,23 +1,37 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
-from .serializers import ScheduleSerializer
-from apps.schedule.models import Schedule
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from apps.schedule.models import Schedule
+from .serializers import ScheduleSerializer
+
+
+def return_query(param):
+    if 'medico' in param.keys() and 'especialidade' in param.keys():
+        return Schedule.objects\
+                .filter(doctor__specialty=param['especialidade'])\
+                .filter(doctor=param['medico'])
+    elif 'especialidade' in param.keys():
+        return Schedule.objects.filter(doctor__specialty=param['especialidade'])
+    elif 'medico' in param.keys():
+        return Schedule.objects.filter(doctor=param['medico'])
 
 
 class ScheduleViewSet(ModelViewSet):
-
     queryset = Schedule.objects.all()
     serializer_class = ScheduleSerializer
     permission_classes = [IsAuthenticated]
     http_method_names = ['get']
 
     def list(self, request, *args, **kwargs):
-        if len(request.query_params) > 0:
-            print(f"Médico: {request.query_params['medico']}")
-            print(f"Especialidade: {request.query_params['especialidade']}")
-            print(f"Data Inicio: {request.query_params['data_inicio']}")
-            print(f"Data final: {request.query_params['data_final']}")
+        querys = request.query_params
         queryset = Schedule.objects.all()
         serializer = ScheduleSerializer(queryset, many=True)
-        return Response(serializer.data)
+        print(querys)
+
+        if len(querys) > 0:
+            schedules = return_query(querys)
+            serializer = ScheduleSerializer(schedules, many=True)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.data)
